18af1fc1a43c6f37a7dcc9b23e06542b9a5da259
takkasila <takkasila@outlook.com>
2015-04-15 18:46:32 +0700

    Import new assets
    Import Standard asset player controller. Added pillar generating script.

---
+++ Assets/Script/PillarGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarGenerator : MonoBehaviour {
+
+	public GameObject pillar;
+	public float radius = 8;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+
+		// z
+		for (int f1=1; f1<= radius * 2; f1++)
+		{
+			// x
+			for(int f2=1; f2<= radius *2; f2++)
+			{
+				Vector3 playerPos = new Vector3(Mathf.Floor(player.transform.position.x) , 0, Mathf.Floor(player.transform.position.z) );
+				Vector3 blockPos = new Vector3(f1 - radius,0,f2 - radius) + playerPos;
+
+				if(Vector3.Distance(blockPos, playerPos) <= radius)
+				{
+					Instantiate(pillar, blockPos, Quaternion.identity);
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
